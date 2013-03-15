78ab8fc65b2193a6c5a4e19ff1fcfbab5cb5f409
Evan W. Isnor <ewisnor@gmail.com>
2013-03-15 10:12:48 -0300

    Initial commit
    Much of the Duration object implementation is done. Tests are present
    with a few failures.

---
+++ js/duration.js
@@ -0,0 +1,218 @@
+'use strict';
+
+var Duration = function(representation) {
+	self = this;
+
+	/* Fields */
+
+	self.seconds = 0;
+
+	/* Constants */
+
+	self.SECONDS_IN_A_MINUTE = 60;
+	self.SECONDS_IN_AN_HOUR = self.SECONDS_IN_A_MINUTE * 60;
+	self.SECONDS_IN_A_DAY = self.SECONDS_IN_AN_HOUR * 24;
+	self.SECONDS_IN_A_WEEK = self.SECONDS_IN_A_DAY * 7;
+	self.SECONDS_IN_A_MONTH = self.SECONDS_IN_A_WEEK * 4;
+	self.SECONDS_IN_A_YEAR = self.SECONDS_IN_A_DAY * 365;
+
+	self.UNEXPECTED_FORMAT_ERROR = "Unexpected duration format. Refer to ISO 8601.";
+	self.UNEXPECTED_FLOAT_ERROR = "Unexpected duration format. Try passing in an Integer or an ISO 8601 Duration string instead.";
+	self.NEGATIVE_VALUE_ERROR = "Cannot create a negative Duration.";
+
+	/* Parsing */
+
+	self.SUPPORTED_FORMAT = {
+		MODULI_DELIMITED : /^P(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})$/,
+		MODULI_NONDELIMITED : /^P(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})$/,
+		NO_MODULI_WEEKS : /^P(\d+W)$/,
+		NO_MODULI : /^P(\d+Y)*(\d+M)*(\d+D)*(?:(T)(\d+H)*(\d+M)*(\d+S)*)?$/
+	};
+
+	self.parse = function(pattern, match) {
+		if (pattern === self.SUPPORTED_FORMAT.NO_MODULI_WEEKS) {
+			for (var i = 1; i < match.length; i++) {
+				var value = match[i];
+				if (/\d+W/.test(value)) {
+					self.seconds += parseInt(value.replace('W', '')) * self.SECONDS_IN_A_WEEK;
+				}
+				else if (/\d+[A-Z]/.test(value)) {
+					console.log("Throwing UNEXPECTED_FORMAT_ERROR");
+					throw new Error(self.UNEXPECTED_FORMAT_ERROR);
+				}
+			}
+		}
+		else if (pattern === self.SUPPORTED_FORMAT.NO_MODULI) {
+			if (match[0] === 'P' || match[0] === 'PT') {
+				console.log("Throwing UNEXPECTED_FORMAT_ERROR");
+				throw new Error(self.UNEXPECTED_FORMAT_ERROR);
+			}
+			
+			var hasFoundT = false;
+			for (var groupIndex = 1; groupIndex < match.length; groupIndex++) {
+				var value = match[groupIndex];
+				if (/T/.test(value)) {
+					hasFoundT = true;
+				}
+				else if (/\d+Y/.test(value)) {
+					self.seconds += parseInt(value.replace('Y', '')) * self.SECONDS_IN_A_YEAR;
+				}
+				else if (/\d+M/.test(value) && !hasFoundT) {
+					self.seconds += parseInt(value.replace('M', '')) * self.SECONDS_IN_A_MONTH;
+				}
+				else if (/\d+D/.test(value)) {
+					self.seconds += parseInt(value.replace('D', '')) * self.SECONDS_IN_A_DAY;
+				}
+				else if (/\d+H/.test(value)) {
+					self.seconds += parseInt(value.replace('H', '')) * self.SECONDS_IN_AN_HOUR;
+				}
+				else if (/\d+M/.test(value) && hasFoundT) {
+					self.seconds += parseInt(value.replace('M', '')) * self.SECONDS_IN_A_MINUTE;
+				}
+				else if (/\d+S/.test(value)) {
+					self.seconds += parseInt(value.replace('S', ''));
+				}
+				else if (/\d+[A-Z]/.test(value)) {
+					console.log("Throwing UNEXPECTED_FORMAT_ERROR");
+					throw new Error(self.UNEXPECTED_FORMAT_ERROR);
+				}
+			}
+		}
+		else if (pattern === self.SUPPORTED_FORMAT.MODULI_DELIMITED
+			|| pattern === self.SUPPORTED_FORMAT.MODULI_NONDELIMITED) {
+			
+			for (var groupIndex = 1; groupIndex < match.length; groupIndex++) {
+				var value = parseInt(match[groupIndex]);
+				if (groupIndex === 1) {
+					self.seconds += value * self.SECONDS_IN_A_YEAR;
+				}
+				else if (groupIndex === 2) {
+					if (value > 12) {
+						console.log("Throwing UNEXPECTED_FORMAT_ERROR");
+						throw new Error(self.UNEXPECTED_FORMAT_ERROR);
+					}
+					self.seconds += value * self.SECONDS_IN_A_MONTH;
+				}
+				else if (groupIndex === 3) {
+					if (value > 31) {
+						console.log("Throwing UNEXPECTED_FORMAT_ERROR");
+						throw new Error(self.UNEXPECTED_FORMAT_ERROR);
+					}
+					self.seconds += value * self.SECONDS_IN_A_DAY;
+				}
+				else if (groupIndex === 4) {
+					if (value > 24) {
+						console.log("Throwing UNEXPECTED_FORMAT_ERROR");
+						throw new Error(self.UNEXPECTED_FORMAT_ERROR);
+					}
+					self.seconds += value * self.SECONDS_IN_AN_HOUR;
+				}
+				else if (groupIndex === 5) {
+					if (value > 60) {
+						console.log("Throwing UNEXPECTED_FORMAT_ERROR");
+						throw new Error(self.UNEXPECTED_FORMAT_ERROR);
+					}
+					self.seconds += value * self.SECONDS_IN_A_MINUTE;
+				}
+				else if (groupIndex === 6) {
+					if (value > 60) {
+						console.log("Throwing UNEXPECTED_FORMAT_ERROR");
+						throw new Error(self.UNEXPECTED_FORMAT_ERROR);
+					}
+					self.seconds += value;
+				}
+			}
+		}
+	}
+
+	/* Constructor */
+
+	if (representation == 'undefined' || representation == undefined || representation == '') {
+		representation = 0;
+	}
+	
+	if (typeof representation === 'number' && representation % 1 != 0) {
+		console.log("Throwing UNEXPECTED_FLOAT_ERROR");
+		throw new Error(self.UNEXPECTED_FLOAT_ERROR);
+	}
+	else if (typeof representation === 'number' && representation < 0) {
+		console.log("Throwing NEGATIVE_VALUE_ERROR");
+		throw new Error(self.NEGATIVE_VALUE_ERROR);
+	}
+	else if (typeof representation === 'number' && representation % 1 == 0) {
+		self.seconds = representation;
+	}
+	else {
+		var isSupportedFormat = false;
+		for (var format in self.SUPPORTED_FORMAT) {
+			if (self.SUPPORTED_FORMAT.hasOwnProperty(format)) {
+				var pattern = self.SUPPORTED_FORMAT[format];
+				if (pattern.test(representation)) {
+					isSupportedFormat = true;
+					console.log(format + ": " + representation);
+					self.parse(pattern, representation.match(pattern));
+					break;
+				}
+			}
+		}
+
+		if (!isSupportedFormat) {
+			console.log("Throwing UNEXPECTED_FORMAT_ERROR");
+			throw new Error(self.UNEXPECTED_FORMAT_ERROR);
+		}
+	}
+
+	/* Cumulative getters */
+
+	self.inSeconds = function() {
+		return self.seconds;
+	}
+
+	self.inMinutes = function() {
+		return self.seconds / self.SECONDS_IN_A_MINUTE;
+	}
+
+	self.inHours = function() {
+		return self.seconds / self.SECONDS_IN_AN_HOUR;
+	}
+
+	self.inDays = function() {
+		return self.seconds / self.SECONDS_IN_A_DAY;
+	}
+
+	self.inWeeks = function() {
+		return self.seconds / self.SECONDS_IN_A_WEEK;
+	}
+
+	self.inMonths = function() {
+		return self.seconds / self.SECONDS_IN_A_MONTH;
+	}
+
+	self.inYears = function() {
+		return self.seconds / self.SECONDS_IN_A_YEAR;
+	}
+
+	/* Formatted getters */
+
+	self.ago = function() {
+	}
+
+	self.remaining = function() {
+	}
+
+	self.inFormat = function(format) {
+	}
+
+	/* Arithmetic */
+
+	self.add = function(other) {
+		console.log("Add: " + self.seconds + " + " + other.seconds);
+		self.seconds += other.inSeconds();
+		return self;
+	}
+
+	self.subtract = function(other) {
+		self.seconds -= other.inSeconds();
+		return self;
+	}
+}
---
+++ test/duration.test.js
@@ -0,0 +1,464 @@
+'use strict';
+
+describe('Duration Test Suite', function() {
+
+	beforeEach(function() {
+
+	});
+
+	afterEach(function() {
+
+	});
+
+	describe('Duration Parsing Tests', function() {
+		
+		beforeEach(function() {
+
+		});
+
+		afterEach(function() {
+
+		});
+		
+		it("should initialize at 0 seconds without parameters", function() {
+			var duration = new Duration();
+			expect(duration.seconds).toBe(0);
+		});
+
+		it("should parse integers as seconds", function() {
+			for (var s = 0; s < 1245; s++) {
+				expect((new Duration(s)).seconds).toBe(s);
+			}
+		});
+		
+		it("should parse P#Y#M#DT#H#M#S format", function() {
+			expect((new Duration("P1Y1M1DT1H1M1S")).seconds).toBe(
+				(60 * 60 * 24 * 365) // Year
+				+ (60 * 60 * 24 * 7 * 4) // Month
+				+ (60 * 60 * 24) // Day
+				+ (60 * 60) // Hour
+				+ (60) // Minute
+				+ 1); // 1 Second
+
+			expect((new Duration("P10Y10M10DT10H10M10S")).seconds).toBe(
+				(60 * 60 * 24 * 365 * 10) // 10 Years
+				+ (60 * 60 * 24 * 7 * 4 * 10) // 10 Months
+				+ (60 * 60 * 24 * 10) // 10 Days
+				+ (60 * 60 * 10) // 10 Hours
+				+ (60 * 10) // 10 Minutes
+				+ 10); // 10 Seconds
+
+			expect((new Duration("P2Y2M2DT2H2M")).seconds).toBe(
+				(60 * 60 * 24 * 365 * 2) // 10 Years
+				+ (60 * 60 * 24 * 7 * 4 * 2) // 10 Months
+				+ (60 * 60 * 24 * 2) // 10 Days
+				+ (60 * 60 * 2) // 10 Hours
+				+ (60 * 2)); // 10 Minutes
+
+			expect((new Duration("P2Y2M2DT2H2M")).seconds).toBe(
+				(60 * 60 * 24 * 365 * 2) // 2 Years
+				+ (60 * 60 * 24 * 7 * 4 * 2) // 2 Months
+				+ (60 * 60 * 24 * 2) // 2 Days
+				+ (60 * 60 * 2) // 2 Hours
+				+ (60 * 2)); // 2 Minutes
+
+			expect((new Duration("P3Y3M3DT3H")).seconds).toBe(
+				(60 * 60 * 24 * 365 * 3) // 3 Years
+				+ (60 * 60 * 24 * 7 * 4 * 3) // 3 Months
+				+ (60 * 60 * 24 * 3) // 3 Days
+				+ (60 * 60 * 3)); // 3 Hours
+
+			expect((new Duration("P4Y4M4D")).seconds).toBe(
+				(60 * 60 * 24 * 365 * 4) // 4 Years
+				+ (60 * 60 * 24 * 7 * 4 * 4) // 4 Months
+				+ (60 * 60 * 24 * 4)); // 4 Days
+
+			expect((new Duration("P5Y5M")).seconds).toBe(
+				(60 * 60 * 24 * 365 * 5) // 5 Years
+				+ (60 * 60 * 24 * 7 * 4 * 5)); // 5 Months
+
+			expect((new Duration("P6Y")).seconds).toBe(
+				(60 * 60 * 24 * 365 * 6)); // 6 Years
+
+			expect((new Duration("P11M11DT11H11M11S")).seconds).toBe(
+				(60 * 60 * 24 * 7 * 4 * 11) // 11 Months
+				+ (60 * 60 * 24 * 11) // 11 Days
+				+ (60 * 60 * 11) // 11 Hours
+				+ (60 * 11) // 11 Minutes
+				+ 11); // 11 Seconds
+
+			expect((new Duration("P12DT12H12M12S")).seconds).toBe(
+				(60 * 60 * 24 * 12) // 12 Days
+				+ (60 * 60 * 12) // 12 Hours
+				+ (60 * 12) // 12 Minutes
+				+ 12); // 12 Seconds
+
+			expect((new Duration("PT13H13M13S")).seconds).toBe(
+				(60 * 60 * 13) // 13 Hours
+				+ (60 * 13) // 13 Minutes
+				+ 13); // 13 Seconds
+
+			expect((new Duration("PT14M14S")).seconds).toBe(
+				(60 * 14) // 14 Minutes
+				+ 14); // 14 Seconds
+
+			expect((new Duration("PT15S")).seconds).toBe(
+				15); // 15 Seconds
+
+			expect((new Duration("PT61S")).seconds).toBe(
+				61); // 61 Seconds
+
+			expect((new Duration("PT61M61S")).seconds).toBe(
+				(60 * 61) // 61 Minutes
+				+ 61); // 61 Seconds
+
+			expect((new Duration("PT25H61M61S")).seconds).toBe(
+				(60 * 60 * 25) // 25 Hours
+				+ (60 * 61) // 61 Minutes
+				+ 61); // 61 Seconds
+
+			expect((new Duration("P400DT25H61M61S")).seconds).toBe(
+				(60 * 60 * 24 * 400) // 400 Days
+				+ (60 * 60 * 25) // 25 Hours
+				+ (60 * 61) // 61 Minutes
+				+ 61); // 61 Seconds
+
+			expect((new Duration("P13M400DT25H61M61S")).seconds).toBe(
+				(60 * 60 * 24 * 7 * 4 * 13) // 13 Months
+				+ (60 * 60 * 24 * 400) // 400 Days
+				+ (60 * 60 * 25) // 25 Hours
+				+ (60 * 61) // 61 Minutes
+				+ 61); // 61 Seconds
+
+			expect((new Duration("P271Y13M400DT25H61M61S")).seconds).toBe(
+				(60 * 60 * 24 * 365 * 271) // 271 Years
+				+ (60 * 60 * 24 * 7 * 4 * 13) // 13 Months
+				+ (60 * 60 * 24 * 400) // 400 Days
+				+ (60 * 60 * 25) // 25 Hours
+				+ (60 * 61) // 61 Minutes
+				+ 61); // 61 Seconds
+
+		});
+
+		it("should parse P#W format", function() {
+			expect((new Duration("P1W")).seconds).toBe(
+				+ (60 * 60 * 24 * 7 * 1)); // 1 Week
+
+			expect((new Duration("P2W")).seconds).toBe(
+				+ (60 * 60 * 24 * 7 * 2)); // 2 Weeks
+
+			expect((new Duration("P5W")).seconds).toBe(
+				+ (60 * 60 * 24 * 7 * 5)); // 5 Weeks
+
+			expect((new Duration("P20W")).seconds).toBe(
+				+ (60 * 60 * 24 * 7 * 20)); // 20 Weeks
+
+			expect((new Duration("P53W")).seconds).toBe(
+				+ (60 * 60 * 24 * 7 * 53)); // 53 Weeks
+
+			expect((new Duration("P243W")).seconds).toBe(
+				+ (60 * 60 * 24 * 7 * 243)); // 243 Weeks
+		});
+
+		it("should parse P[YYYY][MM][DD]T[hh][mm][ss] format", function() {
+			expect((new Duration("P00010101T010101")).seconds).toBe(
+				(60 * 60 * 24 * 365 * 1) // 1 Year
+				+ (60 * 60 * 24 * 7 * 4 * 1) // 1 Month
+				+ (60 * 60 * 24 * 1) // 1 Day
+				+ (60 * 60 * 1) // 1 Hour
+				+ (60 * 1) // 1 Minute
+				+ 1); // 1 Second
+
+			expect((new Duration("P00241202T100447")).seconds).toBe(
+				(60 * 60 * 24 * 365 * 24) // 24 Years
+				+ (60 * 60 * 24 * 7 * 4 * 12) // 12 Months
+				+ (60 * 60 * 24 * 2) // 2 Days
+				+ (60 * 60 * 10) // 10 Hours
+				+ (60 * 4) // 4 Minutes
+				+ 47); // 47 Seconds
+		});
+
+		it("should parse P[YYYY]-[MM]-[DD]T[hh]:[mm]:[ss] format", function() {
+			expect((new Duration("P0001-01-01T01:01:01")).seconds).toBe(
+				(60 * 60 * 24 * 365 * 1) // 1 Year
+				+ (60 * 60 * 24 * 7 * 4 * 1) // 1 Month
+				+ (60 * 60 * 24 * 1) // 1 Day
+				+ (60 * 60 * 1) // 1 Hour
+				+ (60 * 1) // 1 Minute
+				+ 1); // 1 Second
+
+			expect((new Duration("P0024-12-02T10:04:47")).seconds).toBe(
+				(60 * 60 * 24 * 365 * 24) // 24 Years
+				+ (60 * 60 * 24 * 7 * 4 * 12) // 12 Months
+				+ (60 * 60 * 24 * 2) // 2 Days
+				+ (60 * 60 * 10) // 10 Hours
+				+ (60 * 4) // 4 Minutes
+				+ 47); // 47 Seconds
+		});
+
+		it ("should fail to parse improperly formatted duration strings", function() {
+			expect(function() {
+				new Duration("this is garbage data lol");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+			
+			expect(function() {
+				new Duration("*(^#$JDJ34h fw5ad..6fjw#D 343");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+
+			expect(function() {
+				new Duration("P20S");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+
+			expect(function() {
+				new Duration("P3Y6W");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+
+			expect(function() {
+				new Duration("P3Y6S");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+
+			expect(function() {
+				new Duration("P4Z9JT2S");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+
+			expect(function() {
+				new Duration("P271Y-13M-400DT25H:61M:61S");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+
+			expect(function() {
+				new Duration("P0001-01-01T01:01");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+
+			expect(function() {
+				new Duration("P01-01T01:01:01");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+
+			expect(function() {
+				new Duration("P0001-01-01");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+
+			expect(function() {
+				new Duration("PT01:01:01");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+
+			expect(function() {
+				new Duration("PT01:01");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+
+			expect(function() {
+				new Duration("P0001-13-01T01:01:01");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+
+			expect(function() {
+				new Duration("P0001-01-32T01:01:01");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+
+			expect(function() {
+				new Duration("P0001-01-01T25:01:01");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+
+			expect(function() {
+				new Duration("P0001-01-01T01:61:01");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+
+			expect(function() {
+				new Duration("P0001-01-01T01:01:61");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+
+			expect(function() {
+				new Duration("P0001-13-32T25:61:61");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+
+			expect(function() {
+				new Duration("P");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+
+			expect(function() {
+				new Duration("PT");
+			}).toThrow(Duration.UNEXPECTED_FORMAT_ERROR);
+		});
+	});
+
+	describe('Duration Constants Tests', function() {
+		var duration;
+
+		beforeEach(function() {
+			duration = new Duration();
+		});
+
+		afterEach(function() {
+
+		});
+		
+		it("should know how many seconds are in a minute", function() {
+			expect(duration.SECONDS_IN_A_MINUTE).toBe(60);
+			// expect(Duration.SECONDS_IN_A_MINUTE).toBe(60);
+		});
+		
+		it("should know how many seconds are in an hour", function() {
+			expect(duration.SECONDS_IN_AN_HOUR).toBe(60 * 60);
+			// expect(Duration.SECONDS_IN_AN_HOUR).toBe(60 * 60);
+		});
+		
+		it("should know how many seconds are in a day", function() {
+			expect(duration.SECONDS_IN_A_DAY).toBe(60 * 60 * 24);
+			// expect(Duration.SECONDS_IN_A_DAY).toBe(60 * 60 * 24);
+		});
+		
+		it("should know how many seconds are in a week", function() {
+			expect(duration.SECONDS_IN_A_WEEK).toBe(60 * 60 * 24 * 7);
+			// expect(Duration.SECONDS_IN_A_WEEK).toBe(60 * 60 * 24 * 7);
+		});
+		
+		it("should know how many seconds are in a month", function() {
+			expect(duration.SECONDS_IN_A_MONTH).toBe(60 * 60 * 24 * 7 * 4);
+			// expect(Duration.SECONDS_IN_A_MONTH).toBe(60 * 60 * 24 * 7 * 4);
+		});
+		
+		it("should know how many seconds are in a year", function() {
+			expect(duration.SECONDS_IN_A_YEAR).toBe(60 * 60 * 24 * 356);
+			// expect(Duration.SECONDS_IN_A_YEAR).toBe(60 * 60 * 24 * 356);
+		});
+	});
+
+	describe('Duration Cumulative Getter Tests', function() {
+		var duration;
+
+		beforeEach(function() {
+			duration = new Duration("P10Y10M10DT10H10M10S");
+		});
+
+		afterEach(function() {
+
+		});
+		
+		it("should return the correct cumulative number of seconds", function() {
+			expect(duration.inSeconds()).toBe(
+				(60 * 60 * 24 * 365 * 10) // 10 Years
+				+ (60 * 60 * 24 * 7 * 4 * 10) // 10 Months
+				+ (60 * 60 * 24 * 10) // 10 Days
+				+ (60 * 60 * 10) // 10 Hours
+				+ (60 * 10) // 10 Minutes
+				+ 10); // 10 Seconds
+		});
+		
+		it("should return the correct cumulative number of minutes", function() {
+			expect(duration.inMinutes()).toBe(
+				((60 * 60 * 24 * 365 * 10) // 10 Years
+				+ (60 * 60 * 24 * 7 * 4 * 10) // 10 Months
+				+ (60 * 60 * 24 * 10) // 10 Days
+				+ (60 * 60 * 10) // 10 Hours
+				+ (60 * 10) // 10 Minutes
+				+ 10) / 60); // 10 Seconds
+				//Divided by the number of seconds in a minute to give us the total minutes
+		});
+		
+		it("should return the correct cumulative number of hours", function() {
+			expect(duration.inHours()).toBe(
+				((60 * 60 * 24 * 365 * 10) // 10 Years
+				+ (60 * 60 * 24 * 7 * 4 * 10) // 10 Months
+				+ (60 * 60 * 24 * 10) // 10 Days
+				+ (60 * 60 * 10) // 10 Hours
+				+ (60 * 10) // 10 Minutes
+				+ 10) / 60 / 60); // 10 Seconds
+				//Divided by the number of minutes in an hour to give us the total hours
+		});
+		
+		it("should return the correct cumulative number of days", function() {
+			expect(duration.inDays()).toBe(
+				((60 * 60 * 24 * 365 * 10) // 10 Years
+				+ (60 * 60 * 24 * 7 * 4 * 10) // 10 Months
+				+ (60 * 60 * 24 * 10) // 10 Days
+				+ (60 * 60 * 10) // 10 Hours
+				+ (60 * 10) // 10 Minutes
+				+ 10) / 60 / 60 / 24); // 10 Seconds
+				//Divided by the number of hours in a day to give us the total days
+		});
+		
+		it("should return the correct cumulative number of weeks", function() {
+			expect(duration.inWeeks()).toBe(
+				((60 * 60 * 24 * 365 * 10) // 10 Years
+				+ (60 * 60 * 24 * 7 * 4 * 10) // 10 Months
+				+ (60 * 60 * 24 * 10) // 10 Days
+				+ (60 * 60 * 10) // 10 Hours
+				+ (60 * 10) // 10 Minutes
+				+ 10) / 60 / 60 / 24 / 7); // 10 Seconds
+				//Divided by the number of weeks in a year to give us the total weeks
+			
+		});
+		
+		it("should return the correct cumulative number of months", function() {
+			expect(duration.inMonths()).toBe(
+				((60 * 60 * 24 * 365 * 10) // 10 Years
+				+ (60 * 60 * 24 * 7 * 4 * 10) // 10 Months
+				+ (60 * 60 * 24 * 10) // 10 Days
+				+ (60 * 60 * 10) // 10 Hours
+				+ (60 * 10) // 10 Minutes
+				+ 10) / 60 / 60 / 24 / 7 / 4); // 10 Seconds
+				//Divided by the number of weeks in a month to give us the total months
+		});
+		
+		it("should return the correct cumulative number of years", function() {
+			expect(duration.inYears()).toBe(
+				((60 * 60 * 24 * 365 * 10) // 10 Years
+				+ (60 * 60 * 24 * 7 * 4 * 10) // 10 Months
+				+ (60 * 60 * 24 * 10) // 10 Days
+				+ (60 * 60 * 10) // 10 Hours
+				+ (60 * 10) // 10 Minutes
+				+ 10) / 60 / 60 / 24 / 365); // 10 Seconds
+				//Divided by the number of days in a year to give us the total years
+		});
+	});
+
+	describe('Duration Formatted Getter Tests', function() {
+		
+		beforeEach(function() {
+
+		});
+
+		afterEach(function() {
+
+		});
+		
+		it("should return a formatted duration as specified", function() {
+
+		});
+		
+		it("should return a formatted duration in terms of how long ago something was", function() {
+
+		});
+		
+		it("should return a formatted duration in terms of how long something has been occurring", function() {
+
+		});
+	});
+
+	describe('Duration Arithmetic Tests', function() {
+		
+		beforeEach(function() {
+
+		});
+
+		afterEach(function() {
+
+		});
+		
+		it("should add various other durations", function() {
+			console.log('-------------- Add 1 + 1');
+			expect(((new Duration(1)).add(new Duration(1))).seconds).toBe(2);
+			console.log('-------------- Add PT1S + 1');
+			expect(((new Duration("PT1S")).add(new Duration(1))).seconds).toBe(2);
+			console.log('-------------- Add PT1M + 1');
+			expect(((new Duration('PT1M')).add(new Duration(1))).seconds).toBe(61);
+			console.log('-------------- Add PT1M + P2D');
+			expect(((new Duration("PT1M")).add(new Duration("P2D"))).seconds).toBe(60 + (60 * 60 * 24 * 2));
+		});
+		
+		it("should subtract various other durations", function() {
+			expect(((new Duration(1)).subtract(new Duration(1))).seconds).toBe(0);
+			expect(((new Duration("PT1S")).subtract(new Duration(1))).seconds).toBe(0);
+			expect(((new Duration("PT1M")).subtract(new Duration(1))).seconds).toBe(59);
+			expect(((new Duration("P2D")).subtract(new Duration("P1D"))).seconds).toBe(60 * 60 * 24);
+		});
+	});
+});
