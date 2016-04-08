5a69608929816ba9aea4d74b295507aad978a99e
Josh Birdwell <jbirdwell34@gmail.com>
2016-04-08 16:13:52 -0500

    Fix the parentheses

--- js/duration.js
+++ js/duration.js
@@ -327,16 +327,16 @@ Duration.prototype.ago = function() {
 		return Math.floor(this.inHours()) + ' hour' + ((Math.floor(this.inHours()) > 1) ? 's' : '') + ' ago';
 	}
 	else if (this.seconds < this.Calendar.Seconds.per.Week) {
-		return Math.floor(this.inDays()) + ' day' + ((Math.floor(this.inDays(()) > 1) ? 's' : '') + ' ago';
+		return Math.floor(this.inDays()) + ' day' + ((Math.floor(this.inDays()) > 1) ? 's' : '') + ' ago';
 	}
 	else if (this.seconds < this.Calendar.Seconds.per.Month) {
 		return Math.floor(this.inWeeks()) + ' week' + ((Math.floor(this.inWeeks()) > 1) ? 's' : '') + ' ago';
 	}
 	else if (this.seconds < this.Calendar.Seconds.per.Year) {
-		return Math.floor(this.inMonths()) + ' month' + ((Math.floor(this.inMonths() > 1)) ? 's' : '') + ' ago';
+		return Math.floor(this.inMonths()) + ' month' + ((Math.floor(this.inMonths()) > 1) ? 's' : '') + ' ago';
 	}
 	else {
-		return Math.floor(this.inYears()) + ' year' + (Math.floor((this.inYears() > 1)) ? 's' : '') + ' ago';
+		return Math.floor(this.inYears()) + ' year' + ((Math.floor(this.inYears()) > 1) ? 's' : '') + ' ago';
 	}
 }
 
